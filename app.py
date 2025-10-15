cf9d6c16a4a0e9e50cab8990979118fd97ab98a5
Deyra Herrera <a385845@uach.mx>
2025-10-15 07:49:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import math
 
-# TRIÁNGULO 
+
 st.title("Calcular el área y permímetro de figuras")
 figura = st.selectbox("Selecciona la figura", ["Triángulo"])
-
+#TRIÁNGULO
 if figura == "Triángulo":
     st.subheader("Parámetros del triángulo")
     base = st.number_input("Base (b)", min_value=0.0, format="%.2f")
@@ -12,9 +12,8 @@ if figura == "Triángulo":
     lado_a = st.number_input("Lado a", min_value=0.0, format="%.2f")
     lado_b = st.number_input("Lado b", min_value=0.0, format="%.2f")
     lado_c = st.number_input("Lado c", min_value=0.0, format="%.2f")
-        area = 0.5 * base * altura
-        perimetro = lado_a + lado_b + lado_c
-
-        st.metric("Área", f"{area:.2f}")
-        st.metric("Perímetro", f"{perimetro:.2f}")
-        st.success("Resultados")
+    area = 0.5 * base * altura
+    perimetro = lado_a + lado_b + lado_c
+    st.metric("Área", f"{area:.2f}")
+    st.metric("Perímetro", f"{perimetro:.2f}")
+    st.success("Resultados")
