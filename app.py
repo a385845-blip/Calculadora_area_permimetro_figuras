e9a2a48b0f9662d19e61ef52f19a7ffd1f587a08
Deyra Herrera <a385845@uach.mx>
2025-10-15 07:39:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import math
 
 st.title("Calcular el área y permímetro de figuras")
-figura = st.selectbox("Selecciona la figura", ["triangulo, circulo, rectangulo, cuadrado"])
+figura = st.selectbox("Selecciona la figura", ["triangulo"])
 
